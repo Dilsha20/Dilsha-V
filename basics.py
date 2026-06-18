8cdacdd27f8a00f14ca119135bfe254957f9ee34
Dilsha20 <dilshaminnu3@gmail.com>
2026-06-18 09:03:38 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,32 @@
+# variable
+# comment
+# print("hello")
+# ctrl+?
+
+# variablrs=value
+
+a=5
+b=12
+# print(a)
+my_age=21
+
+# print(age)
+
+# swap 2 numbers
+
+
+c = a
+a = b
+b = c
+print(a)
+print(b)
+
+# operators
+a=5
+b=12
+a=a+10
+a=a+b  #12
+b=a-b #5
+a=a-b #7
+
+
