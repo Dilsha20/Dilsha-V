716feb1ba6cd24c726f7ce46f352d550ced0e8d2
Dilsha20 <dilshaminnu3@gmail.com>
2026-07-16 23:12:51 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,45 @@
+#lambda function
+# anonymous fuction
+
+# lambda argumenets : expression
+
+# def add2(a,b):
+#     return a+b
+# print(add2(1,4))
+
+
+# z= lambda x,y :x+y
+# print(z(2,3))
+
+# product of 3 numbers
+# z= lambda a,b,c : a*b*c
+# print(z(4,3,5))
+
+# square of a number
+# z=lambda x,y :x**y
+# print(z(2,3))
+
+# perimeter of a circle
+# z=lambda r : 2*3.14*r
+# print(z(6))
+
+# area of a triangle
+# z=lambda r:3.14*r**r
+# print(z(3))
+
+# square root of a number
+# z=lambda x: x**0.5
+# print(z(2))
+
+# full name of a person
+# z=lambda fname,lname:fname+" "+lname
+# print(z("Dilsha","musthafa"))
+
+#eligibility checking
+# check= lambda age:"eligible" if age>=18 else "not eligible"
+# print(check(19))
+
+# current age
+# a=lambda current,born:current-born
+# print(a(2026,2005))
+
