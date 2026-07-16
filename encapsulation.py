1cdc439164751c5184e58791855593312fc1ed13
Dilsha20 <dilshaminnu3@gmail.com>
2026-07-16 23:09:12 +0530

    Add files via upload

---
+++ encapsulation.py
@@ -0,0 +1,11 @@
+# encapsulation
+
+#  #access modifiers
+# 1.public -- same class , same package , sub class , other packages
+# 2.protected( _ ) -- same class , same package , sub class 
+# eg:-- class student():
+#             def _hello(self):
+                  
+# 3.private ( __) --  same class
+#  eg:-- class student():
+#              def __hello(self):   
