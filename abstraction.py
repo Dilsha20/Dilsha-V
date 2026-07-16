9c4b2c1728cef6bea4471c85c6917c270c46f2a9
Dilsha20 <dilshaminnu3@gmail.com>
2026-07-16 23:07:08 +0530

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,18 @@
+#abstraction
+#data hiding
+#possible in through inheritence
+
+from abc import ABC,abstractmethod
+class Animal(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def make_sound(self):
+        print("animal make sound")
+class Dog(Animal):
+    def __init__(self):
+        pass
+    def make_sound(self):
+        print("woff woff woff")
+d=Dog()
+d.make_sound()                
