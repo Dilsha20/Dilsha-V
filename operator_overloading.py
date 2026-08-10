cd0d27f98db91603077fe9039574ed9f7b17cde1
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:28:01 +0530

    Create operator_overloading.py

---
+++ operator_overloading.py
@@ -0,0 +1,11 @@
+ #operator overloading
+# # class Student:
+# #     def __init__(self,m1,m2):
+# #         self.m1=m1
+# #         self.m2=m2
+# #     def __add__(self, otr):
+# #         return self.m1+self.m2,otr.m1+otr.m2
+
+# # s1=Student(7,8)
+# # s2=Student(9,7)           
+# # print(s1+s2)
