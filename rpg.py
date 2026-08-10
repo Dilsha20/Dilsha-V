8c41c51b82a70f006d3567588fd89bcd59c94a52
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:17:42 +0530

    Create rpg.py

---
+++ rpg.py
@@ -0,0 +1,25 @@
+#create an rpg game
+# import random
+# player=100
+# eneymy=100
+# while player>0 and eneymy>0:
+#     print("player health:-",player)
+#     print("enemy health:-",eneymy)
+#     print("1. attack")
+#     print("2. heal")
+#     choice = input("enter your choice:-")
+#     if choice=="1":
+#         damage=random.randint(10,20)
+#         eneymy-=damage
+#         print(f"you attacked the enemy and dealt {damage} damage")
+#     elif choice=="2":
+#         heal=random.randint(10,20)
+#         player+=heal
+#         print(f"you healed yourself for {heal} health")
+#     else:
+#         print("invalid choice")
+    
+#     if eneymy>0:
+#         enemy_damage=random.randint(5,15)
+#         player-=enemy_damage
+#         print(f"enemy attacked you and dealt {enemy_damage} damage")
