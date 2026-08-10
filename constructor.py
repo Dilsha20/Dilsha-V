c94770c963af36b8501882388b7d440a19a5cc6a
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:24:05 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,18 @@
+#constructor
+#used to initialize an object
+#__init__()
+#self using to point the current object reffering
+# class Car:
+#     def __init__(self,n,c):
+#         self.name=n
+#         self.color=c
+#     def start(self):
+#         print(f"{self.name} has started")
+#     def stop(self):
+#         print("car stopped")
+# c1=Car("swift","black")
+# c2=Car("city","red")
+# c3=Car
+# c4=Car
+# c1.start()
+# c1.stop() 
