e412534008d51d22878a3e0420d9e449eaaee21e
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:06:23 +0530

    Create calculator_func.py

---
+++ calculator_func.py
@@ -0,0 +1,34 @@
+#create calculator using functions
+# def add(a,b):
+#     return a+b
+
+# def sub(a,b):
+#     return a-b
+
+# def mul(a,b):
+#     return a*b
+
+# def div(a,b):
+#     return a/b
+
+# def main():
+#     while True:
+#         print("Welcome to simple calculator !!!!!!!!!!!!!!")
+#         ch=int(
+#             input("enter your choice : \n1.Add\n2.Sub\n3.Mul\n4.Div\n5.Exit\n:----"))
+#         x=int(input("enter number :"))
+#         y=int(input("enter number :"))
+#         if ch == 1:
+#             print(add(x,y))
+#         elif ch == 2:
+#             print(sub(x,y))
+#         elif ch == 3:
+#             print(mul(x,y))
+#         elif ch == 4:
+#             print(div(x,y)) 
+#         elif ch == 5:
+#             break
+#         else:
+#             print("invalid choice")
+# main()                   
+
