11b5ca05de18d71cdc28e340423e9e932c9b5336
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:05:15 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,87 @@
+
+#for i in range():
+      #code to be executed
+
+# for i in range(1,10):
+#     print(i)
+
+
+# for a in range(1,10,2):
+#     print(a)    
+
+
+
+# #sum of 100 numbers
+# sum=0
+# for i in range(1,101,1):
+#     sum=sum+i
+# print(sum)
+
+
+# #reverse of a number
+
+# for i in range(10,0,-1):
+#     print(i)
+
+
+# #factorial of a number 
+#     fact=1
+#     for i in range(5,1,-1):
+#         fact=fact*i
+#         i=i-1
+# print(fact)        
+
+
+# #multiplication of a number
+# num=5
+# for i in range(1,11):
+#      print(i)
+
+
+# #sum and reverse of a number
+
+# count=int(input("enter num count:"))
+# sum=0
+# for i in range(count):
+#     num=int(input ("enter your number:-"))
+#     sum=sum+num
+# print(f"sum is {sum}")
+# print(f"average={sum/count}")
+
+
+
+# #check if a number is amrstron or not
+# #153
+
+# num=15345
+# count=0
+# while a>0:
+#     count=count+1
+#     num=num//10
+#     print(count)
+
+
+
+#break continue and pass
+#break- stop loop
+#continue- skip next itration on the loop
+#pass- jumb next program
+
+#pass- 
+# a=19
+# if age >=18:
+#     pass
+
+# continue
+
+# for i in range(1,11):
+#     if i==6:
+#         continue
+#     print(i)
+
+
+#break
+for i in range(1,11):
+    if i==6:
+        break
+    print(i)
