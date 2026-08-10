e47bccc03aeef60df96dec908d26832542e043ea
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:32:40 +0530

    Add files via upload

---
+++ statement.py
@@ -0,0 +1,44 @@
+#conditional statement
+
+#if condition:
+   # code to be executed
+
+
+#if
+age=34
+if age>=18:
+     print("eligible")
+else:
+     print("not eligible") 
+
+a=5
+b=7
+if(a>10):
+     print("a is greater than 5")
+else:
+     print("not its not")
+
+
+#check if a number is odd or even 
+num=12346
+if(num%2==0):
+     print("even")
+else:
+     print("odd")   
+
+
+#check if a number is a multiple of 5
+num=1050
+if(num%5==0):
+     print("correct")
+else:
+     print("not")
+
+#largest of two numbers  
+a=5
+b=7
+if(a<b):
+     print("a is larger")
+else:
+     print("b is larger")   
+
