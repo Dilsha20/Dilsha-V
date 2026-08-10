2ecfed2b7efe1eea1501e054b4abc482391acd88
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:16:47 +0530

    Create rock_paper_scissor.py

---
+++ rock_paper_scissor.py
@@ -0,0 +1,25 @@
+ #rock paper scissor
+# import random
+# ch=("rock","paper","scissor")
+# comp=random.choice(ch)
+# player=""
+# while player not in ch:
+#     player=input("enter your choice rock/paper/scissor:---")
+# print(f"player:- {player}\ncomputer:-{comp}")
+# if player==comp:
+#     print("its a tieee")
+# elif comp=="rock":
+#     if player=="scissor":
+#         print("rock smashes scissor computer wins")
+#     else:
+#         print("paper covers rock player wins")
+# elif comp=="paper":
+#     if player=="rock":
+#        print("paper covers rock computer wins")
+#     else:
+#         print("rock beats scissor player wins")   
+# elif comp=="scissor":
+#     if player=="paper":
+#         print("scissor cut the paper player wins")
+#     else:
+#         print("rock beats scissor comp wins")        
