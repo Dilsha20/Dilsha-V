275e7773874b0a0b70a4f9a66d09e847ef88295d
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:09:43 +0530

    Create multi_level_inheritance.py

---
+++ multi_level_inheritance.py
@@ -0,0 +1,25 @@
+ #multi level
+# class Person3(Person2):
+#     def __init__(self):
+#         pass
+#     def fly(self):
+#         print("person can fly")
+#     def swim(self):
+#         print("peson can swim")
+#     def speak(self):
+#         print("person 3 can speak")
+
+# class Person4(Person3):
+#     def __init__(self):
+#         pass
+#     def sleep(self):
+#         print("person can sleep")
+#     def eat(self):
+#         print("peson can eat")
+#     def speak(self):
+#         print("person 4 can speak")
+#         super().speak()
+
+# p3=Person3()
+# p4=Person4()
+# p4.speak() #method overriding
