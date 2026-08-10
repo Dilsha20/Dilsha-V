a6970b78ece5fdcb7f7764ad168d062b26f0d7c3
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:15:23 +0530

    Add files via upload

---
+++ map&filter.py
@@ -0,0 +1,36 @@
+#map & filter
+
+#map
+#adds an itereable to a function
+# maps each items in an itreable to a function
+# map(fun,itreable)
+# def square(z):
+#     return z**
+
+# products=[
+#     ("nike x11",178),
+#     ("apple watch",456),
+#     ("samsung s24",999),
+#     ("ps5 pro",700),
+#     ("iphone",1100)
+# ]
+
+# def toINR(z):
+#     return z[1]*96.34
+
+# result=map(toINR,products)
+# print(list(result))
+ 
+
+# z=lambda x:x**2    
+# a=[1,2,3,4,5,6,7,8,9,10]    
+# result=map(z,a)
+# print(list(result))
+
+#filter
+
+
+# def iseven(a):
+#     if a%2==0:
+#         return a 
+# a=[1,2,3,4,5,6,7,8,9,10] 
