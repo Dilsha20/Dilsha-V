9d100efde8790190feb23c8ede000612076dbf48
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:26:06 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,78 @@
+#arithmetic operator
+
+#addition
+a=7
+b=10
+print(a+b)
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#division
+print(a/b)
+
+#modulus
+print(a%b)
+
+
+
+#comparison operator
+#true or false
+
+
+#equal to
+a=7
+b=9
+print(a==b)
+
+#not equal to
+print(a!=b)
+
+#greaterthan
+print(b>a)
+
+#lessthan
+print(b<a)
+
+#greaterthan or equal to
+print(a>=b)
+
+#lessthan or equal to
+print(a<=b)
+
+
+#logical operators
+# True,True =True
+# True,False=False
+# False,True=False
+# False,False=False
+  
+
+#and
+a=7
+b=10
+if a>5 and b==10:
+    print('yes')
+
+#or
+a=6
+b=7
+if(a>1 or b>15 and a==6 or b=='das'):
+     print('yes')
+
+#not
+a=10
+print(not a==10)
+
+#in operator or membership operator
+a="hari"
+if "a" in a:
+     print("yes")
+if "ai" in a:
+     print("yes")
+else:
+     print("no")
+     
