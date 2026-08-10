fa2129327e9953b2bf8c54c6cb1d518998ae384e
Dilsha20 <dilshaminnu3@gmail.com>
2026-08-10 13:11:06 +0530

    Create single_level_inheritance.py

---
+++ single_level_inheritance.py
@@ -0,0 +1,27 @@
+#single level
+# class Person1:
+#     def __init__(self):
+#         pass
+#     def walk(self):
+#         print("person can walk")
+#     def smile(self):
+#         print("person can smile hahahahahahahaha")
+#     def speak(self):
+#         print("person 1 can speak")
+
+# class Person2(Person1):
+#     def __init__(self):
+#         pass
+#     def read(self):
+#         print("person can read")
+#     def write(self):
+#         print("peson can write")
+#     def speak(self):
+#         print("person 2 can speak")    
+
+# p1=Person1()
+# p2 = Person2()
+# p1.walk()
+# p2.smile()                    
+# p2.read()
+# p2.write()
